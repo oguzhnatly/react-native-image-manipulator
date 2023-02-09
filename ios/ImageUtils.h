736f4b54390c186427487847218e1d174c0c744a
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2023-02-09 16:20:29 +0100

    feat: add ios impl with extracting method code to .m to avoid objcpp conflicts

--- ios/ImageUtils.h
+++ ios/ImageUtils.h
@@ -1,9 +1,14 @@
 #import <UIKit/UIKit.h>
 #import <CoreMedia/CoreMedia.h>
 #import <Foundation/Foundation.h>
+#import <React/RCTBridge.h>
 
 @interface ImageUtils : NSObject
 
 + (UIImage *)cropImage:(UIImage *)image toRect:(CGRect)rect;
-
++ (void)manipulate:(NSString *)uri
+          actions:(NSArray *)actions
+          saveOptions:(NSDictionary *)saveOptions
+          resolver:(RCTPromiseResolveBlock)resolve
+          rejecter:(RCTPromiseRejectBlock)reject;
 @end
